75e97ba3fbb71dcf0e52f4441641a306188bba7b
Yen Ying Ng <nyysang@hotmail.com>
2023-06-03 20:43:24 +0200

    Model changed from Keras to Torch

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Importing required libs
 from flask import Flask, render_template, request
 from model import preprocess_img, predict_result
+from PIL import Image
+
 
 # Instantiating flask app
 app = Flask(__name__)
@@ -17,15 +19,30 @@ def main():
 def predict_image_file():
     try:
         if request.method == 'POST':
-            img = preprocess_img(request.files['file'].stream)
-            pred = predict_result(img)
+            img_stream = request.files['file'].stream
+            tensor_img = preprocess_img(img_stream)
+            pred = predict_result(tensor_img)
             return render_template("result.html", predictions=str(pred))
 
-    except:
-        error = "File cannot be processed."
-        return render_template("result.html", err=error)
+    except Exception as e:
+        msg = "error: "+str(e)
+        return render_template("result.html", err=msg)
+
+
+
+def image_info(img):
+    # img = Image.open(img_stream)
+            
+    # Get basic image information
+    image_format = img.format
+    image_size = img.size
+            
+    # Construct a string with the image information
+    info_str = f"Format: {image_format}<br>"
+    info_str += f"Size: {image_size}<br>"
+    return info_str
 
 
 # Driver code
 if __name__ == "__main__":
-    app.run(port=9000, debug=True)
+    app.run(host='0.0.0.0', port=9000, debug=True)
--- model.py
+++ model.py
@@ -1,15 +1,66 @@
 # Importing required libs
-from keras.models import load_model
-from keras.utils import img_to_array
 import numpy as np
 from PIL import Image
 
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing.image import img_to_array
+# from tensorflow.keras.applications import MobileNet
+
+
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import json
+
+# Load the class labels
+with open('models/imagenet-simple-labels.json', 'r') as f:
+    classes = json.load(f)
+
+model = models.vgg16(pretrained=True)
+model.eval()
+
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+
+
 # Loading model
-model = load_model("D:/SACHIN/Models/Hand-Sign-Digit-Language/digit_model.h5")
+# model = load_model('models/digit_model.h5')
+# model = MobileNet(weights='imagenet')
 
 
 # Preparing and pre-processing the image
-def preprocess_img(img_path):
+
+def preprocess_img(img_stream):
+    image = Image.open(img_stream)
+    input_tensor = preprocess(image)
+    input_batch = input_tensor.unsqueeze(0)
+    return input_batch
+
+# Make predictions
+def predict_result(input_batch):
+   
+    # return str(input_batch)
+    with torch.no_grad():
+        output = model(input_batch)
+
+    # Get the predicted class index
+    _, predicted_idx = torch.max(output, 1)
+    predicted_label = "xxxx"
+    predicted_label = classes[predicted_idx.item()]
+
+    # Print the predicted label
+    print('Predicted Label:', predicted_label)
+    return predicted_label
+
+
+
+def preprocess_img_old(img_path):
     op_img = Image.open(img_path)
     img_resize = op_img.resize((224, 224))
     img2arr = img_to_array(img_resize) / 255.0
@@ -18,6 +69,7 @@ def preprocess_img(img_path):
 
 
 # Predicting function
-def predict_result(predict):
-    pred = model.predict(predict)
-    return np.argmax(pred[0], axis=-1)
+def predict_result_old(img):
+    # pred = model.predict(img)
+    # result = np.argmax(pred[0], axis=-1)
+    return "dupa"
