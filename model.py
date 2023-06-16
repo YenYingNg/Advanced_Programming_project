faff90c901f75f291669bcf75bebdb66165cb527
Yen Ying Ng <nyysang@hotmail.com>
2023-06-16 11:22:52 +0200

    adding architecture and modify the css file

--- model.py
+++ model.py
@@ -1,18 +1,72 @@
 # Importing required libraries
 from PIL import Image
 import torch
-import torchvision.models as models
 import torchvision.transforms as transforms
+import torch.nn as nn
 
-import json
+# define the model architecture
+class CatsDogsClassifier(nn.Module):
+    def __init__(self):
+        super(CatsDogsClassifier, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 2),
+        )
 
-# Load the class labels
-with open('models/imagenet-simple-labels.json', 'r') as f:
-    classes = json.load(f)
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
 
-# model = models.vgg16(weights="IMAGENET1K_V1")
-model = models.mobilenet_v3_small(weights="IMAGENET1K_V1")
+# Create an instance of the model
+model = CatsDogsClassifier()
+
+# Load the model
+state_dict = torch.load('C:/Users/adamp/Desktop/Advanced_Programming_project-master/checkpoint.pth')
+model.load_state_dict(state_dict)
 model.eval()
+
 
 preprocess = transforms.Compose([
     transforms.Resize(256),
@@ -37,7 +91,8 @@ def predict_result(img_stream):
         output = model(img_tensor)
 
     # Get the predicted class index
+    classes = ['cat', 'dog']
     _, predicted_idx = torch.max(output, 1)    
-    predicted_label = classes[predicted_idx.item()]
+    predicted_label = classes[predicted_idx]
     return predicted_label
 
